380200bbc5f08bafad1ffa834d02602b59d8d043
Ritik Mishra <ritikmishra25419@gmail.com>
2026-06-30 21:41:00 +0530

    fix: configure Prisma 7 client with MariaDB driver adapter

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const adapter = new PrismaMariaDb({
+  uri: process.env.DATABASE_URL as string,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
